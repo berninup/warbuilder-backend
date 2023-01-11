7f2adc1bf212b3c654b1471ad931468ffd8fc4bd
berninup <benrincb@gmail.com>
2023-01-10 21:53:18 -0600

    created find by id, delete, and update unit routes

--- controllers/unitRouter.js
+++ controllers/unitRouter.js
@@ -18,10 +18,22 @@ unitRouter.delete('/:id', (req, res) => {
 })
 
 // Update Unit TODO
+unitRouter.put('/:id', (req, res) => {
+    Unit.findByIdAndUpdate(req.params.id, req.body, (error, updatedUnit) => {
+        res.json(updatedUnit)
+    })
+})
 
 // Create New Unit
 unitRouter.post('/', (req, res) => {
     Unit.create(req.body, (error, unit) => {
+        res.json(unit)
+    })
+})
+
+// Find Unit by ID
+unitRouter.get('/:id', (req,res) => {
+    Unit.findById(req.params.id, (error, unit) => {
         res.json(unit)
     })
 })
